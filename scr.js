625c021b533a5b794c7dac341293a75b131d995f
islami00 <islamkwandi@gmail.com>
2021-05-06 08:55:51 +0100

    finished ctrl logic, next style

--- scr.js
+++ scr.js
@@ -1,41 +1,82 @@
-var modulo,document, img,toggleActive,current,next,previous;
+var modulo,document,leftbutton,rbutton,img,toggleActiveImg,currentImg,next,previous,skipTo,buttonSpan,buttonRem;
+// for expandability, use js to create slider buttons all to length of image array
+// then replace three with image array length
 
-leftbutton = document.querySelector(".slider-left-button")
-rbutton = document.querySelector(".slider-right-button")
-img = document.querySelectorAll(".slider-content")
+leftbutton = document.querySelector(".slider-left-button");
+rbutton = document.querySelector(".slider-right-button");
+img = document.querySelectorAll(".slider-content");
+buttonSpan  = document.querySelectorAll(".slider-button");
 
-
-toggleActive = function (a) {
+toggleActiveImg = function (a) {
     a.classList.toggle("active-image");
-}
-
+};
+toggleActiveSel = function (a) {
+    a.classList.toggle("active-button");
+};
 modulo = function(number,mod){
     return ((number % mod)+mod) % mod;
 };
 
-current = 0
+currentImg = 0;
 
 next = function(array) {
-    current = modulo(current,3);
-    toggleActive(array[current]);
-    current+=1;
-    current = modulo(current,3);
-    toggleActive(array[current])
-}
+    currentImg = modulo(currentImg,3);
+    toggleActiveImg(array[currentImg]);
+    currentImg+=1;
+    currentImg = modulo(currentImg,3);
+    toggleActiveImg(array[currentImg]);
+
+};
 
 
 previous = function(array) {
-    current = modulo(current,3);
-    toggleActive(array[current]);
-    current-=1;
-    current = modulo(current,3);
-    toggleActive(array[current])
+    currentImg = modulo(currentImg,3);
+    toggleActiveImg(array[currentImg]);
+    currentImg-=1;
+    currentImg = modulo(currentImg,3);
+    toggleActiveImg(array[currentImg]);
+
+};
+
+buttonRem = function(){
+    // button remove
+    for (let sIndex = 0; sIndex < buttonSpan.length; sIndex++) {
+        if (buttonSpan[sIndex].classList = "slider-button active-button"){
+            toggleActiveSel(buttonSpan[sIndex])
+        }
+    }
 }
 
+skipTo = function(currSelIndex,imgArray) {
+    // image change over
+    toggleActiveImg(imgArray[currentImg])
+    currentImg = currSelIndex
+    toggleActiveImg(imgArray[currSelIndex])
+    // button change over
+        buttonRem();
+        // button switch
+        toggleActiveSel(buttonSpan[currSelIndex])
+};
+
+// add listener for slider control
+for (let i = 0; i < buttonSpan.length; i++) {
+    buttonSpan[i].addEventListener("click", e =>{
+        skipTo(i,img);
+    });
+};
+
 leftbutton.addEventListener("click", e =>{
-    next(img)
-})
+    previous(img);
+    // button remove
+    buttonRem();
+    // switch button to curr
+    toggleActiveSel(buttonSpan[currentImg])
+});
 rbutton.addEventListener("click", e =>{
-    previous(img)
-})
+    next(img);
+    // button remove
+    buttonRem();
+    // switch button to curr
+    toggleActiveSel(buttonSpan[currentImg])
+});
 
